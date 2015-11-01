f01c8e674e5a966b3970c9faf395917afb6c4351
Christian Sciberras <christian@sciberras.me>
2015-11-02 00:11:59 +0100

    Conceptual prototype

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ScintillaNET_Kitchen
+﻿namespace Scintilla_Theme_Designer
 {
     partial class Form1
     {
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.scintilla1 = new ScintillaNET.Scintilla();
+            this.scintilla2 = new ScintillaNET.Scintilla();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scintilla1
+            // 
+            this.scintilla1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scintilla1.Lexer = ScintillaNET.Lexer.Null;
+            this.scintilla1.Location = new System.Drawing.Point(12, 12);
+            this.scintilla1.Name = "scintilla1";
+            this.scintilla1.Size = new System.Drawing.Size(865, 431);
+            this.scintilla1.TabIndex = 0;
+            this.scintilla1.UseTabs = false;
+            // 
+            // scintilla2
+            // 
+            this.scintilla2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scintilla2.Lexer = ScintillaNET.Lexer.Cpp;
+            this.scintilla2.Location = new System.Drawing.Point(12, 449);
+            this.scintilla2.Name = "scintilla2";
+            this.scintilla2.ReadOnly = true;
+            this.scintilla2.Size = new System.Drawing.Size(1210, 236);
+            this.scintilla2.TabIndex = 1;
+            this.scintilla2.UseTabs = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(886, 39);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.ShowCellErrors = false;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(336, 404);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 135.0254F;
+            this.Column1.HeaderText = "Token";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = "...";
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.FillWeight = 64.97462F;
+            this.Column2.HeaderText = "Style";
+            this.Column2.Name = "Column2";
+            this.Column2.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Null"});
+            this.comboBox1.Location = new System.Drawing.Point(922, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(883, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Lexer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1234, 697);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.scintilla2);
+            this.Controls.Add(this.scintilla1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ScintillaNET Theme Designer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ScintillaNET.Scintilla scintilla1;
+        private ScintillaNET.Scintilla scintilla2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewButtonColumn Column2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,17 +4,145 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ScintillaNET_Kitchen
+namespace Scintilla_Theme_Designer
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            scintilla1.Text = "";
+            comboBox1.Items.AddRange(Enum.GetNames(typeof(ScintillaNET.Lexer)));
+            comboBox1.Text = "Null";
+
+            scintilla2.StyleResetDefault();
+            scintilla2.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            scintilla2.Styles[ScintillaNET.Style.Default].Size = 10;
+            scintilla2.StyleClearAll();
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Default].ForeColor = Color.Silver;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla2.Styles[ScintillaNET.Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintilla2.Styles[ScintillaNET.Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Number].ForeColor = Color.Olive;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Word].ForeColor = Color.Blue;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Word2].ForeColor = Color.Blue;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintilla2.Styles[ScintillaNET.Style.Cpp.StringEol].BackColor = Color.Pink;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Operator].ForeColor = Color.Purple;
+            scintilla2.Styles[ScintillaNET.Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+
+            this.UpdateResult();
+        }
+
+        protected void UpdateResult()
+        {
+            var text = new StringBuilder();
+            var lexerName = Enum.GetName(typeof(ScintillaNET.Lexer), scintilla1.Lexer);
+            var defaultStyle = scintilla1.Styles[ScintillaNET.Style.Default];
+            var styleType = defaultStyle.GetType();
+
+            // TODO maybe get this from styleType?
+            var styleKeys = new string[] {
+                "BackColor", "Bold", "Case", "FillLine", "Font", "ForeColor", "Hotspot", "Italic", "Size", "SizeF", "Underline", "Visible", "Weight",
+            };
+
+            text.AppendLine("scintilla1.Lexer = ScintillaNET.Lexer." + lexerName + ";");
+            text.AppendLine("");
+
+            foreach (var item in this.GetLexerStyles(scintilla1.Lexer))
+            {
+                foreach (var styleKey in styleKeys)
+                {
+                    var oldValue = styleType.GetProperty(styleKey).GetValue(defaultStyle);
+                    var newValue = styleType.GetProperty(styleKey).GetValue(scintilla1.Styles[item.Key]);
+                    if (oldValue.ToString() != newValue.ToString() && !(styleKey == "Font" && newValue.ToString() == ""))
+                    {
+                        var serializedValue = this.SerializeValue(newValue);
+                        if (serializedValue != null)
+                        {
+                            text.AppendLine("scintilla1.Styles[ScintillaNET.Style." + lexerName + "." + item.Value + "]." + styleKey + " = " + serializedValue + ";");
+                        }
+                    }
+                }
+            }
+
+            scintilla2.ReadOnly = false;
+            scintilla2.Text = text.ToString();
+            scintilla2.ReadOnly = true;
+        }
+
+        private Dictionary<string, Func<object, string>> typeSerializers = new Dictionary<string, Func<object, string>>()
+        {
+            { "null", m => null },
+            { typeof(Color).FullName, m => ((Color)m).IsNamedColor ? "Color." + ((Color)m).Name : "Color.FromName(\"" + ((Color)m).Name + "\")" },
+            { typeof(Boolean).FullName, m => (Boolean)m ? "true" : "false" },
+            { typeof(String).FullName, m => "\"" + m.ToString() + "\"" },
+            { typeof(int).FullName, m => m.ToString() },
+            { typeof(float).FullName, m => m.ToString() },
+            { typeof(ScintillaNET.StyleCase).FullName, m => "ScintillaNET.StyleCase." + Enum.GetName(typeof(ScintillaNET.StyleCase), m) },
+        };
+
+        private string SerializeValue(object value)
+        {
+            var typeName = value == null ? "null" : value.GetType().FullName;
+
+            if (!this.typeSerializers.ContainsKey(typeName))
+            {
+                throw new NotImplementedException("Cannot serialize value of type " + value.GetType().Name + ".");
+            }
+
+            return this.typeSerializers[typeName](value);
+        }
+
+        private Dictionary<int, string> GetLexerStyles(ScintillaNET.Lexer lexer)
+        {
+            var lexerName = Enum.GetName(typeof(ScintillaNET.Lexer), lexer);
+            var lexerType = Type.GetType(typeof(ScintillaNET.Style).AssemblyQualifiedName.Replace(".Style", ".Style+" + lexerName));
+            return lexerType == null
+                ? new Dictionary<int, string>()
+                : lexerType
+                    .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                    .Where(fi => fi.IsLiteral && !fi.IsInitOnly)
+                    .ToDictionary(
+                        fi => (int)fi.GetRawConstantValue(),
+                        fi => fi.Name
+                    );
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            scintilla1.Lexer = (ScintillaNET.Lexer)Enum.Parse(typeof(ScintillaNET.Lexer), comboBox1.Text);
+
+            dataGridView1.Rows.Clear();
+
+            var lexerType = Type.GetType(typeof(ScintillaNET.Style).AssemblyQualifiedName.Replace(".Style", ".Style+" + comboBox1.Text));
+
+            if (lexerType != null)
+            {
+                dataGridView1.Rows
+                    .AddRange(
+                        this
+                            .GetLexerStyles(scintilla1.Lexer)
+                            .Select(m =>
+                            {
+                                var row = new DataGridViewRow();
+                                row.HeaderCell.Value = m.Key.ToString();
+                                row.Cells.Add(new DataGridViewTextBoxCell() { Value = m.Value });
+                                return row;
+                            })
+                            .ToArray()
+                    );
+            }
+
+            this.UpdateResult();
         }
     }
 }
